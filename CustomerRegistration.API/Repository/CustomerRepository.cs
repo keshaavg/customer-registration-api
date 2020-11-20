005287ea7193d5dc5d74aa0757dd7e65c163327d
Keshav Goel <keshav.goel@clekt.co.uk>
2020-11-20 14:15:27 +0000

    Added more comments

--- CustomerRegistration.API/Repository/CustomerRepository.cs
+++ CustomerRegistration.API/Repository/CustomerRepository.cs
@@ -6,15 +6,28 @@ using System.Threading.Tasks;
 
 namespace CustomerRegistration.API.Repository
 {
+    /// <summary>
+    /// <see cref="CustomerRepository"/> which saves customers data using <see cref="CustomerContext"/>
+    /// and handles db errors
+    /// </summary>
     public class CustomerRepository : ICustomerRepository
     {
         private readonly CustomerContext _context;
 
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        /// <param name="context"></param>
         public CustomerRepository(CustomerContext context)
         {
             _context = context ?? throw new ArgumentNullException("context", "DB Context is not initialised"); ;
         }
 
+        /// <summary>
+        /// Add customer to database if customer with same id exist in database raise error. 
+        /// </summary>
+        /// <param name="customer"><see cref="Customer"/></param>
+        /// <returns>If success returs newly assigned customer id else 0</returns>
         public async Task<int> Add(Customer customer)
         {
             try
@@ -29,6 +42,10 @@ namespace CustomerRegistration.API.Repository
             }
         }
 
+        /// <summary>
+        /// Gets all the customer from database for test purpose only
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<Customer>> GetAll()
         {
             return await _context.Customers.ToListAsync();
